f5e88da52cde3141780403b31c25b77ee746bcaf
manabled <31279071+manabled@users.noreply.github.com>
2017-09-27 21:36:33 -0400

    Add files via upload

--- 04-image-puzzle.py
+++ 04-image-puzzle.py
@@ -1,98 +1,133 @@
-import sys, pygame, random
+import sys, pygame, random, os, PIL
 
 assert sys.version_info >= (3,4), 'This script requires at least Python 3.4'
 
-''' pygame stuff '''
-#let pygame set itself up
+
 pygame.init()
+pygame
 
-#window dimensions easy to divide by four
 size = (width,height) = (800,800)
-#puzzle dimensions
 dimensions = (rows,columns) = (4,4)
-
 font = pygame.font.SysFont("arial",64)
-#initialize the window
 screen = pygame.display.set_mode(size)
 
 
-''' Square object '''
-class Square:
+class Square(pygame.sprite.Sprite):
 	color = ''
 	label = ''
-	position = (-1,-1)
-	size = (0,0)
+	position = (-1,-1),
+	dim = (0,0)
+	height = 0
 	visible = True
 	
-	def __init__(self, x, y, width, height):
+	def __init__(self, x, y, w, h):
 		self.position = (x,y)
-		self.size = (width,height)
-	
+		self.dim = (w,h)
+		pygame.sprite.Sprite.__init__(self)
+		self.image = pygame.image.load(os.path.join("C:\\Users\\mason\\OneDrive\\GitHub\\04-image-puzzle\\cream-disreali-gears.jpg"))
+		self.rect = self.image.get_rect()
+		self.rect.topleft = (self.position[0] * 200, self.position[1] * 200)
+
+	def update(self):
+		self.rect.topleft = (self.position[0] * 200, self.position[1] * 200)
+
 	def check_proximity(self, xy):
-		''' take a x/y position (as a tuple) and see if it is next to the current position '''
+		if self.position == (-1,-1): return False
+		if self.position == xy: return False
+		if (abs(xy[0] - self.position[0]) <= 1 and xy[1] == self.position[1]) or (abs(xy[1] - self.position[1]) <= 1 and xy[0] == self.position[0]):
+			return True
 		return False
 	
 	def swap_position(self, xy):
-		''' move to new x/y (tuple) position '''
+		self.position = xy
+		self.rect.topleft = (self.position[0] * 200, self.position[1] * 200)
 	
 	def in_correct_position(self, pos):
-		''' check if self.position lines up with which square this is in the list '''
 		return False
 	
 	def draw_square(self, draw, screen):
-		''' add the square to the draw object '''
 		if self.visible:
 			(x1,y1) = self.position
-			(w,h) = self.size
+			(w,h) = self.dim
 			(x,y) = (x1 * w,y1 * h)
-			draw.rect(screen, self.color, (x,y,w,h))
+			self.rect.topleft = (x * 200, y * 200)
 			f = font.render(self.label,True,(0,0,0))
 			(fwidth,fheight) = font.size(self.label)
-			#center the font
 			(fx,fy) = (x + (w - fwidth)/2,y + (h - fheight)/2)
 			screen.blit(f,(fx,fy))
 		return draw
 
 
-''' Other helper functions '''
 def calculate_xy(pos,puzzle):
-	''' calculates which square is the target '''
-	(w,h) = (width / columns, height / rows)
-	#the magic of integer division: we can figure out which square a mouse click happens in
-	return (int(pos[0]//w),int(pos[1]//h))
+	w = width / columns
+	h = height / rows
+	to_return = (int(pos[0]//w),int(pos[1]//h))
+	return to_return
+
 
 def randomize_puzzle(count,puzzle):
-	''' mix up the puzzle, so it can be played; count represents how much we mix it '''
+	for e in puzzle:
+		if not e.visible:
+			for c in range(count):
+				xy = (x,y) = (e.position[0] + random.randint(-1,1),e.position + random.randint(-1,1))
+				if (x >= 0 and x< columns) and (y >= 0 and y< rows):
+					if e.check_proximity(xy):
+						for p in puzzle:
+							if p.position == xy:
+								p.swap_position(e.position)
+								e.swap_position(xy)
 	return puzzle
 
+
 def draw_puzzle(puzzle):
-	''' draw the puzzle on the screen '''
+	screen.fill((0,0,0))
+	for i in range(len(puzzle)):
+		puzzle[i].draw_square(pygame.draw,screen)
+	pygame.display.flip()
 
 
-''' Main code body '''
-#colors are in RGB format (0–255 for each value represents the intensity of the mixture of red, green, and blue, respectively)
-#If you need pretty colors, I like the library at https://yeun.github.io/open-color/
-colors = [(255,0,0)]	
-
-#build puzzle
+colors = [(0,206,209),(0,255,255),(127,255,212),(0,255,127),(124,252,0),(255,255,0),(255,165,0),(255,0,0),(160,32,240),(218,112,214),(255,105,180),(255,215,0),(240,230,140),(50,205,50),(205,92,92)]
 puzzle = []
 count = 0
-#instantiate an object. It is initialized with a x/y which-square position and a width/height (in pixels)
-puzzle.append(Square(0,0,width/columns,height/rows))
-puzzle[0].color = colors[count % len(colors)]
-puzzle[0].label = str(count+1)
-puzzle[0].visible = True
 
+for j in range(columns):
+	for i in range(rows):
+		temp = Square(i, j, width / columns, height / rows)
+		temp.color = colors[count % len(colors)]
+		count = count + 1
+		temp.label = str(count)
+
+puzzle[len(puzzle) - 1].visible = False
 puzzle = randomize_puzzle(500,puzzle)
+screen = pygame.display.set_mode(size)
 
 moves = 0
 draw_puzzle(puzzle)
 
-''' The Game Loop '''
-while True:
+winning = False
+while not winning:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT: sys.exit()
 		# handle MOUSEBUTTONUP
 		if event.type == pygame.MOUSEBUTTONUP:
 			pos = pygame.mouse.get_pos()
 			xy = calculate_xy(pos,puzzle)
+			print(xy)
+			for e in puzzle:
+				if not e.visible:
+					if e.check_proximity(xy):
+						for c in puzzle:
+							if c.position == xy:
+								c.swap_position(e.position)
+								e.swap_position(xy)
+								draw_puzzle(puzzle)
+								moves = moves + 1
+			winning = True
+			for i in range(len(puzzle)):
+				xy = (x,y) = (i % columns, i // rows)
+				if puzzle[i].position != xy:
+					winning = False
+			if winning:
+				for e in puzzle:
+					e.visible = True
+print('You finished in ' + str(moves) + ' moves.')
