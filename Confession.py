8f046d4b177bd6d1aaa573e586c0640018dfac34
catpalm <35228211+catpalm@users.noreply.github.com>
2018-02-24 10:31:35 -0500

    Add files via upload

---
+++ Confession.py
@@ -0,0 +1,20 @@
+import sys, pygame, random, os
+
+assert sys.version_info >= (3,4), 'This script requires at least Python 3.4'
+
+
+print('Hello!')
+print('I apologize for not being able to do this assignment.')
+print('While I do think that this class is incredibly importatnt, it seems that my brain isnt retaining enough knowledge, and it is'
+	  'at this point that it is made clear to me')
+
+print('I need help, either from you or from a tutor if I am going to do well in this class.')
+print('I know a few things about programming, and I understand some concepts, but I am not able to apply them.')
+print('What I mean is that I am not completely learning what I need to learn, except maybe I shouldnt schedule my classes to be'
+			  'at 8 in the morning.')
+print('The Readme file of this project goes into more details than what is spoken here.')
+print('I highly reccomend that you do so, and I hope that I am able to actually acquire and retain knowledge of programming so I can'
+			  'pursue my dreams.')
+
+print('I apologize for my failure in this assignment, I do not want this to happen to me again, and I will do everything I can to make sure'
+	  'I pass this class.')
